10492b39ce3b9d4652454d09834f6ee9e5c8c42f
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-01-28 20:13:26 -0300

    second commit

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var url = 'mongodb://localhost/crud01';
+
+// Make Mongoose use `findOneAndUpdate()`. 
+// Note that this option is `true` by default,
+// you need to set it to false.
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+// Connection establishment
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
