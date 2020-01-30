13a6bfa2b0217ee8373caf669d29becc4319b927
Marcos-Suarez <marcos_augusto_suarez@yahoo.com.br>
2020-01-30 11:09:25 -0200

    readme

--- read.js
+++ read.js
@@ -1,25 +1,66 @@
-require('./models/user');
-require('./config/connection.js');
-var userService = require('./service/user');
+var assert = require("assert");
+// CONNECTION
+var mongoose = require('mongoose');
+// MODEL SCHEMA
+var schema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName: {
+        type: String,
+        default: ''
+    },
+    lastName: {
+        type: String,
+        default: ''
+    },
+    dob: Date,
+    address: String,
+    phone: String,
+    role: String
+});
+//MODEL INSTANCE
+var user = new mongoose.model('User', schema);
+var url = 'mongodb://localhost/crud01';
 
-//
-// read
-//
-var query = {
-    username: "abc@example.com"
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+// Connection establishment
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+var user = mongoose.model('User');
+
+var query = {username: "abc@example.com"};
+
+var service = {
+    findUser: function (query, callback) {
+        return user.findOne(query, callback);
+
+    }
 };
 
+service.findUser = function (query, callback) {
+    return user.findOne(query, callback);
+}
 
-userService.findUser(query, function (error, response) {
+var foo = service.findUser(query, function (error, response) {
     if (error) {
         console.log(error);
         return;
     }
-    if (response) {
-        console.log(response);
-        return;
-    }
     if (!response) {
         console.log('No Data Found');
+        return;
     }
+        assert.equal("abc@example.com", response.username);
+        console.log("PASSOU");
+        return;
 });
+
